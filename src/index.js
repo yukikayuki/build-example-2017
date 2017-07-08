e9a146fcf26bcff16e31ceee8c86abda6948be5a
Matsui Masashi <matsui@kaffeine.jp>
2017-07-09 02:17:31 +0900

    use babel-polyfill

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import 'babel-polyfill'
+
 import hello from './hello'
 
 hello()
 
 const func = () => console.log('func')
+
+const array = [1, 2, 3]
+const hasOne = array.includes(1)
+console.log(hasOne)
