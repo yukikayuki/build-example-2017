c1c430754975c5e5b03cc3b5a0616ae8449abc3a
Matsui Masashi <matsui@kaffeine.jp>
2017-07-09 01:42:14 +0900

    add webpack babel config

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,14 @@ module.exports = {
   output: {
     path: `${__dirname}/dist`,
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
   }
 }
